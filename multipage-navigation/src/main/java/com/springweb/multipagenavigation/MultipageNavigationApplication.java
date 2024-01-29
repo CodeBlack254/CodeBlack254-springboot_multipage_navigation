0a23ae0d5969cbbbd766bfaed9c4a4d7e806511d
CodeBlack254 <deriqrose@gmail.com>
2024-01-29 16:40:56 +0300

    Initial Commit

---
+++ multipage-navigation/src/main/java/com/springweb/multipagenavigation/MultipageNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.springweb.multipagenavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipageNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipageNavigationApplication.class, args);
+	}
+
+}
